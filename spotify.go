58843545c257bc1bd64694da6432d3ed87733e5c
Julian Hofmann <julian.hofmann2805@gmail.com>
2023-11-23 20:19:39 +0100

    Allow multiple history files for spotify

--- spotify.go
+++ spotify.go
@@ -11,64 +11,66 @@ import (
 )
 
 func spotify(sendListen chan<- Listen) error {
-	if pflag.NArg() != 2 {
+	if pflag.NArg() < 2 {
 		pflag.Usage()
 		os.Exit(1)
 	}
-
-	file, err := os.Open(pflag.Arg(1))
-	if err != nil {
-		return fmt.Errorf("open spotify history file: %w", err)
-	}
-	defer file.Close()
-
-	type spotifyListen struct {
-		TS              string `json:"ts"`
-		MSPlayed        int64  `json:"ms_played"`
-		TrackName       string `json:"master_metadata_track_name"`
-		AlbumName       string `json:"master_metadata_album_album_name"`
-		ArtistName      string `json:"master_metadata_album_artist_name"`
-		SpotifyTrackURI string `json:"spotify_track_uri"`
-	}
-	arrScanner, err := newJsonObjectArrayScanner[spotifyListen](file)
-	if err != nil {
-		return fmt.Errorf("read song list from history file: %w", err)
-	}
-	for {
-		song, err := arrScanner.nextObject()
-		if err == io.EOF {
-			break
+	for i := 1; i < pflag.NArg(); i++ {
+		fmt.Printf("Exporting %s...\n", pflag.Arg(i))
+		file, err := os.Open(pflag.Arg(1))
+		if err != nil {
+			return fmt.Errorf("open spotify history file: %w", err)
 		}
+		defer file.Close()
+
+		type spotifyListen struct {
+			TS              string `json:"ts"`
+			MSPlayed        int64  `json:"ms_played"`
+			TrackName       string `json:"master_metadata_track_name"`
+			AlbumName       string `json:"master_metadata_album_album_name"`
+			ArtistName      string `json:"master_metadata_album_artist_name"`
+			SpotifyTrackURI string `json:"spotify_track_uri"`
+		}
+		arrScanner, err := newJsonObjectArrayScanner[spotifyListen](file)
 		if err != nil {
 			return fmt.Errorf("read song list from history file: %w", err)
 		}
+		for {
+			song, err := arrScanner.nextObject()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return fmt.Errorf("read song list from history file: %w", err)
+			}
 
-		listenedAt, err := time.Parse(time.RFC3339, song.TS)
-		if err != nil {
-			return fmt.Errorf("read song list from history file: invalid timestamp '%s': %w", song.TS, err)
-		}
+			listenedAt, err := time.Parse(time.RFC3339, song.TS)
+			if err != nil {
+				return fmt.Errorf("read song list from history file: invalid timestamp '%s': %w", song.TS, err)
+			}
 
-		if song.TrackName == "" || song.ArtistName == "" || song.MSPlayed < 30000 {
-			continue
-		}
+			if song.TrackName == "" || song.ArtistName == "" || song.MSPlayed < 30000 {
+				continue
+			}
 
-		songURL := "https://open.spotify.com/track/" + strings.Split(song.SpotifyTrackURI, ":")[2]
+			songURL := "https://open.spotify.com/track/" + strings.Split(song.SpotifyTrackURI, ":")[2]
 
-		sendListen <- Listen{
-			TrackMetadata: TrackMetadata{
-				Title:  song.TrackName,
-				Album:  song.AlbumName,
-				Artist: song.ArtistName,
-				AdditionalInfo: AdditionalInfo{
-					MusicService:     "spotify.com",
-					SubmissionClient: "export-to-listenbrainz",
-					SpotifyID:        songURL,
-					OriginURL:        songURL,
-					DurationMS:       song.MSPlayed,
+			sendListen <- Listen{
+				TrackMetadata: TrackMetadata{
+					Title:  song.TrackName,
+					Album:  song.AlbumName,
+					Artist: song.ArtistName,
+					AdditionalInfo: AdditionalInfo{
+						MusicService:     "spotify.com",
+						SubmissionClient: "export-to-listenbrainz",
+						SpotifyID:        songURL,
+						OriginURL:        songURL,
+					},
 				},
-			},
-			ListenedAt: listenedAt.Unix(),
+				ListenedAt: listenedAt.Unix(),
+			}
 		}
+		file.Close()
 	}
 	return nil
 }
